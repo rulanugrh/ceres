a7ff7fbdf079c60a110e1ca1d51cafae5677b4fb
rulanugrh <nextarituenak01@gmail.com>
2023-09-10 21:09:42 +0700

    fix: navbar section

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -2,10 +2,17 @@ import React from "react";
 import { Avatar, Link } from '@nextui-org/react'
 import Image from "next/image";
 
+
 export default function Navbar() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const menuItems = [
+        "Organizer",
+        "Player",
+      ];
     return (
         <div className="lg:px-20 px-6">
             <div className="bg-bgColor rounded-2xl backdrop-filter backdrop-blur-lg bg-opacity-25 pt-5 pb-5 shadow-lg shadow-black/40">
+                
                 <div className="flex flex-wrap">
                     <div className="px-6 lg:w-3/4 w-1/2 self-center">
                         <Link href="/" className="text-white font-bold font-firaCode lg:text-xl text-sm">
@@ -13,7 +20,7 @@ export default function Navbar() {
                             <span className="ml-2">IT Club</span>
                         </Link>
                     </div>
-                    <div className="lg:px-12 px-4 lg:w-1/4 w-1/2 space-x-2 lg:space-x-8 lg:self-center">
+                    <div className="lg:px-20 px-4 lg:w-1/4 w-1/2 space-x-2 lg:space-x-8 lg:self-center">
                         <Link href={"/profile"} className="text-white font-bold font-firaCode lg:text-xl text-sm">
                             Hello
                         </Link>
