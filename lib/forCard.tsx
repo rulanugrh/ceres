c79296fa5b3d505577a37f0766c625a0ee97f56d
rulanugrh <nextarituenak01@gmail.com>
2023-09-10 11:36:34 +0700

    feat: button

--- lib/forCard.tsx
+++ lib/forCard.tsx
@@ -9,6 +9,7 @@ interface Props {
     LinkIG: string
     Jobs: string
     TTL: string
+    LinkProfile: string
     Color: "primary" | "success" | "default" | "secondary" | "warning" | "danger"
 }
 
@@ -17,22 +18,27 @@ export default function CardProfile(props: Props) {
         <div className="shadow-md shadow-shadowBox">
             <Card radius="sm" className="max-w-[500px] pt-5 pb-5 px-4 backdrop-filter backdrop-blur-lg bg-opacity-25 bg-bgColor ">
                 <CardHeader className="justify-between mb-2">
-                    <div className="flex flex-wrap ">
-                        <div className="self-center">
-                            <Avatar isBordered size="md" radius="full" color={`${props.Color}`} src={`${props.Image}`} />
-                        </div>
-                        <div className="px-5">
+                    <div className="flex gap-5 ">
+                        <Avatar isBordered size="md" radius="full" color={`${props.Color}`} src={`${props.Image}`} className="self-center"/>
+                        
+                        <div className="px-4 flex flex-col items-start justify-center">
                             <h3 className="font-firaCode text-sm lg:text-xl font-bold leading-none text-shadowBox">{props.Name}</h3>
                             <h4 className="font-firaCode text-sm lg:text-lg tracking-tight text-white">{props.TTL}</h4>
                         </div>
                     </div>
+                    <Button className="text-foreground font-firaCode" color={`${props.Color}`} radius="full" size="md"> 
+                        <a href={`${props.LinkProfile}`}  className="text-white font-firaCode">Ikuti</a>
+                    </Button>
+                    
                 </CardHeader>
                 <CardBody className="px-3 py-0 text-sm lg:text-lg font-firaCode text-gray-400">
                     <p>{props.Desc}</p>
                     <span className="pt-2 pb-2">{props.Jobs}</span>
                 </CardBody>
-                <CardFooter>
-                    <a href={`${props.LinkIG}`} className="hover:text-blue-500  text-white lg:text-lg text-sm font-firaCode font-thin">{props.Uname}</a>
+                <CardFooter> 
+                    <div className="w-1/2">
+                        <a href={`${props.LinkIG}`} className="hover:text-blue-500  text-white lg:text-lg text-sm font-firaCode font-thin">{props.Uname}</a>
+                    </div> 
                 </CardFooter>
             </Card>
         </div>
